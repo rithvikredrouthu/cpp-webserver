04d01ddf12933efd8f5d4011f2ed333ca46bd0da
rithvikr <rithvik.redrouthu@gmail.com>
2024-06-19 16:00:38 -0400

    Registered mount point for static files

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include "httplib.h"
 #include <iostream>
-using namespace httplib;
 
 int main(void) {
   std::cout << "Webserver started!" << std::endl;
-  Server svr;
+  httplib::Server server;
 
-  svr.Get("/hi", [](const Request &, Response &res) {
-    res.set_content("Hello World!", "text/plain");
+  // Register static directory
+  server.set_mount_point("/www-data", "app/www-data");
+
+  server.Get("/", [&](const httplib::Request &req, httplib::Response &res) {
+    res.set_content(
+      "<html><head><title>Index page</title></head><body><h1>Hello, World!</h1></body></html>", 
+      "text/html"
+    );
   });
 
-  svr.listen("0.0.0.0", 8080);
+  server.Get("/test", [&](const httplib::Request &req, httplib::Response &res) {
+    res.set_content("Test page: Hello, World!", "text/plain");
+  });
+
+  server.Get("/stop", [&](const httplib::Request& req, httplib::Response& res) {
+    std::cout << "Webserver stopped!" << std::endl;
+    server.stop();
+    res.set_redirect("/");
+  });
+
+  server.listen("0.0.0.0", 8080);
 }
